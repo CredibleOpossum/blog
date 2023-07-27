6c16869364932d3b95dfb6bd8e9df043f63d3525
CredibleOpossum <CredibleOpossum@protonmail.com>
2023-07-27 07:19:30 -0400

    add title, change articles.json, use search params

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,44 @@
 function go_to_blog(id) {
-    var converter = new showdown.Converter();
-    converter.setOption("ghCodeBlocks", true)
-    converter.setFlavor('github');
-    fetch(`collection/${id}/markup`)
-        .then((res) => res.text())
-        .then((text) => {
-            document.getElementById("link_container").remove();
-            var html = converter.makeHtml(text);
-            document.getElementById('blog_container').insertAdjacentHTML('beforeend', html);
-            hljs.highlightAll();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('id', id);
+    window.location.search = urlParams;
+}
+async function fetch_json() {
+    return fetch(`collection/articles.json`)
+        .then((res) => res.json())
+        .then(json => {
+            return json;
         })
         .catch((e) => console.error(e))
 }
-fetch(`collection/articles.json`)
-    .then((res) => res.text())
-    .then((text) => {
-        var blogs = JSON.parse(text);
-        for (const link in blogs) {
-            var html = `<a href="javascript:void(0);" onClick="go_to_blog(${blogs[link]})">${link}</a> <br>`
-            document.getElementById("link_container").innerHTML += html;
-        }
-    })
-    .catch((e) => console.error(e))
+
+async function main_behavior() {
+    // TODO: Handle bad IDs
+    var blog_json = await fetch_json() // We need title if we are rendering a page, if not we need to show the links
+
+    url = new URL(window.location.href);
+    if (url.searchParams.has('id')) {
+        var id = new URLSearchParams(window.location.search).get("id");
+        document.title = blog_json[id];
+        var converter = new showdown.Converter();
+        converter.setOption("ghCodeBlocks", true)
+        converter.setFlavor('github');
+        fetch(`collection/${id}/markup`)
+            .then((res) => res.text())
+            .then((text) => {
+                document.getElementById("link_container").remove();
+                var html = converter.makeHtml(text);
+                document.getElementById('blog_container').insertAdjacentHTML('beforeend', html);
+                hljs.highlightAll();
+            })
+            .catch((e) => console.error(e))
+        return;
+    }
+
+    for (const link in blog_json) {
+        var html = `<a href="javascript:void(0);" onClick="go_to_blog(${link})">${blog_json[link]}</a> <br>`
+        document.getElementById("link_container").innerHTML += html;
+    }
+
+}
+main_behavior();
